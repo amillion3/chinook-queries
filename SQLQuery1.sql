a72e6e6b236681845e17c6ab89ca80580517c6e9
Andy Million <amillion3@gmail.com>
2018-10-16 21:15:19 -0500

    exercise 13 complete

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -121,10 +121,18 @@ select
 from Track
 	join InvoiceLine on Track.TrackId = InvoiceLine.TrackId
 
-
 /************************************************/
 -- #13
 -- line_item_track_artist.sql: Provide a query that includes the purchased track name AND artist name with each invoice line item.
+
+select 
+	InvoiceLineId,
+	Track.Name,
+	Artist.Name
+from InvoiceLine
+	join Track on InvoiceLine.TrackId = Track.TrackId
+		join Album on Track.AlbumId = Album.AlbumId
+			join Artist on Album.ArtistId = Artist.ArtistId
 
 /************************************************/
 -- #14
